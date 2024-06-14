ddbcdd10db70fa702c644c5546b95ab2e1a7c562
ainunmulyana <76513223+ainunmulyana3748@users.noreply.github.com>
2024-06-14 23:06:50 +0700

    add file final project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
+  },
+  plugins: [],
+};
